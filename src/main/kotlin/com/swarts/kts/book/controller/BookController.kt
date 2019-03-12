b0b799637399ca69fd5cd6a525c94139be0ed6fb
sinaneski <sinan@swarts.co.uk>
2019-03-12 00:22:14 +0000

    Add logging using github.microutils

--- src/main/kotlin/com/swarts/kts/book/controller/BookController.kt
+++ src/main/kotlin/com/swarts/kts/book/controller/BookController.kt
@@ -3,29 +3,48 @@ package com.swarts.kts.book.controller
 import com.swarts.kts.book.dto.BookRequest
 import com.swarts.kts.book.dto.BookResponse
 import com.swarts.kts.book.service.BookService
+import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class BookController(private val service: BookService) {
 
+    private val log = KotlinLogging.logger {}
+
     @GetMapping(value = ["/books"])
     fun getBooks() : List<BookResponse> {
 
-        return service.getBooks()
+        log.info { "Request all books" }
+
+        val books = service.getBooks()
+
+        log.info { "$books.size books returned" }
+
+        return books
     }
 
     @GetMapping(value = ["/books/{isbn}"])
     fun getBooks(@PathVariable(name = "isbn") isbn: String) : BookResponse {
 
-        return service.getBookByIsbn(isbn)
+        log.info { "Request book with isbn = $isbn" }
+
+        val book = service.getBookByIsbn(isbn)
+
+        log.info { "Returned book info: $book" }
+
+        return book
     }
 
     @PostMapping(value = ["books"])
     @ResponseStatus(HttpStatus.CREATED)
     fun saveBook(@RequestBody book: BookRequest) {
 
+        log.info { "Request to add book = $book" }
+
         service.saveBook(book)
+
+        log.info { "Book successfully added." }
     }
 
 }
